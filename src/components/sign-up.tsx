4919deacfd2c66f89b0b9baaa0e18d1b1fb0ed78
swaraj bachu <rajswaraj.r@gmail.com>
2024-01-23 17:36:23 +0530

    tailwind merge

--- src/components/sign-up.tsx
+++ src/components/sign-up.tsx
@@ -4,10 +4,10 @@ import TextInput from './basic/text-input'
 export default function SignUp() {
   return (
     <section>
-      <h2 className='text-xl'>Signup a driver</h2>
+      <h2 className='text-xl font-semibold mb-6'>Signup a driver</h2>
       <div className='flex gap-3'>
         <TextInput label='Enter Full Name' id='fullName' placeholder='Jonny Ive' />
-        <TextInput label='Enter Email' id='email' />
+        <TextInput label='Enter Email' id='email' placeholder='John@gmail.com' />
       </div>
     </section>
   )
