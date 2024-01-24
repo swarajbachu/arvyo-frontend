c5d2fcccb9fbfda88ab6521d8f7f4aa4d63b33fe
swaraj bachu <rajswaraj.r@gmail.com>
2024-01-24 21:58:46 +0530

    minor change

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -12,7 +12,7 @@ import VechileInformation from "@/components/vechile-information";
 export default function SignUpPage() {
   return (
     <section className="min-h-screen w-full">
-      <h3 className="text-sm text-blue-600">Register Driver</h3>
+      <h3 className="text-sm text-blue-600 ml-5">Register Driver</h3>
       <Divider className="my-2" />
       <div className="p-6 lg:flex gap-6">
         <div className="w-full lg:w-3/5 lg:pr-5 lg:border-r">
