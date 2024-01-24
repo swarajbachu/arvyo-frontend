9d96b6661f661a52b43d53eb970bf00043bf9ebc
swaraj bachu <rajswaraj.r@gmail.com>
2024-01-24 22:00:22 +0530

    also added signup button to home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -62,11 +62,18 @@ export default function Home() {
           Logout
         </button>
       ) : (
+        <div className="flex gap-2">
         <button
         onClick={() => router.push("/login")}
         className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
           Login
         </button>
+        <button
+        onClick={() => router.push("/signup")}
+        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+          Signup
+        </button>
+        </div>
       )}
     </div>
   );
