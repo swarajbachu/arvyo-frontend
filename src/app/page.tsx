c2cf481d912b95cc0bff922258977f9c6c2b1cb9
swaraj bachu <rajswaraj.r@gmail.com>
2024-01-26 07:49:10 +0530

    button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Button } from "@/components/basic";
 import { auth } from "@/utils/firebase";
 import { User, onAuthStateChanged, signOut } from "firebase/auth";
 import { useRouter } from "next/navigation";
@@ -57,33 +58,13 @@ export default function Home() {
       </h3>
       {authUser ? (
         <div className="flex gap-2">
-          <button
-            onClick={userSignOut}
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-          >
-            Logout
-          </button>
-          <button
-            onClick={() => router.push("/dashboard")}
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-          >
-            Dashboard
-          </button>
+          <Button onClick={userSignOut}>Logout</Button>
+          <Button onClick={() => router.push("/dashboard")}>Dashboard</Button>
         </div>
       ) : (
         <div className="flex gap-2">
-          <button
-            onClick={() => router.push("/login")}
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-          >
-            Login
-          </button>
-          <button
-            onClick={() => router.push("/signup")}
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-          >
-            Signup
-          </button>
+          <Button onClick={() => router.push("/login")}>Login</Button>
+          <Button onClick={() => router.push("/signup")}>Signup</Button>
         </div>
       )}
     </div>
--- src/components/basic/index.ts
+++ src/components/basic/index.ts
@@ -1,10 +1,12 @@
 import Divider from "./divider";
 import TextInput from "./text-input";
 import { PhoneInput } from "./phone-input";
+import Button from "./button";
 
 
 export {
   Divider,
   TextInput,
   PhoneInput,
+  Button
 };
